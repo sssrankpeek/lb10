6fbe4a9d93e6c1d9f30bf8177887c3ea12693a29
Dionis Letnikov <113114648+sssrankpeek@users.noreply.github.com>
2022-11-06 19:41:18 +0300

    Add files via upload

--- lb10/lb10/Source.c
+++ lb10/lb10/Source.c
@@ -27,11 +27,52 @@ double sinus(double o)
 	q = sin(o);
 	return q;
 }
+double factorial(int u)//сколько раз выполнитс€ факториал
+{
+	int b = 1;
+	for (int i = 1; i <= u; i++)
+	{
+		b *= i;
+	}
+	return b;
+}
+double sin_n(double x, int N)
+{
+	x = x * M_PI / 180;
+	double y = 0;
+	for (int k = 1; k <= N; k++)
+	{
+		y += (pow(-1, (k - 1)) * pow(x,2*k-1))/factorial(2*k-1);
+	}
+	return y;
+}
+double sin_eps(double x, double eps) 
+{
+	x = x * M_PI / 180;
+	double y = 0;
+	int k = 1;
+	while (fabs(y) > eps);
+	{
+		y += (pow(-1, (k - 1)) * pow(x, 2 * k - 1)) / factorial(2 * k - 1);
+		k++;
+	}
+	return y;
+}
+double ln(double x, double n)
+{
+	double lnx = 0;
+	for (int k = 0; k <= n; k++)
+	{
+		lnx += 2 * ((pow((x - 1), (2*k + 1))) / ((2*k+1)*pow((x+1),(2*k+1))));
+	}
+	return lnx;
+}
+
 void main()
 {
 	setlocale(LC_ALL, "RUS");
-	int x;
-	double o;
+	int x,N,n,h;
+	double o,eps;
 	printf("¬ведите значение х:");
 	scanf("%d", &x);
 	printf("\nfa(x)=%lf , fb(x)=%lf ", fa(x), fb(x));
@@ -39,5 +80,16 @@ void main()
 
 	printf("\nвведите х:");
 	scanf("%lf", &o);
-	printf("sinus %lf gradusov raven %lf", o, sinus(o));
+	printf("\n¬ведите число N - суммируемых k-x членов р€да:");
+	scanf("%d",&N);
+	printf("\nsinus %lf gradusov raven %lf", o, sinus(o));
+	printf("\nsinus %lf gradusov po ryadu teylora raven %lf", o, sin_n(o,N));
+
+	printf("\nведите точность:");
+	scanf("%lf", &eps);
+	printf("\nsinus %lf gradusov v sin_eps raven %lf", o, sin_eps(o, eps));
+
+	printf("\nвведите число x и n:");
+	scanf("%d%d", &h,&n);
+	printf("\nln(x)=%lf",ln(h,n));
 }
